13d6975271926df34759aac996695d055337221e
Bryan <bryanlima200@outlook.com>
2022-11-15 00:35:05 -0300

    Configurações iniciais do formulário de cadastro de novo usuário

--- src/app/conta/conta.module.ts
+++ src/app/conta/conta.module.ts
@@ -9,6 +9,9 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './login/login.component';
 
 import { ContaRoutingModule } from './conta.route';
+import { ContaService } from './services/conta.service';
+
+import { CustomFormsModule } from 'ngx-custom-validators';
 
 @NgModule({
   declarations: [
@@ -22,7 +25,11 @@ import { ContaRoutingModule } from './conta.route';
     ContaRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    CustomFormsModule
+  ],
+  providers: [
+    ContaService
   ]
 })
 export class ContaModule { }
