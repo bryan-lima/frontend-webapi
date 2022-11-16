e9bc9589e71576fceb8de0de616023cdcf2be47e
Bryan <bryanlima200@outlook.com>
2022-11-15 22:38:37 -0300

    Iniciado implementação do image cropper

--- src/app/produto/novo/novo.component.ts
+++ src/app/produto/novo/novo.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators, FormControlName } from '@angular/fo
 import { Router } from '@angular/router';
 
 import { Observable, fromEvent, merge } from 'rxjs';
+import { ImageCroppedEvent, ImageTransform, Dimensions } from 'ngx-image-cropper';
 
 import { utilsBr } from 'js-brasil';
 import { ToastrService } from 'ngx-toastr';
@@ -20,6 +21,17 @@ import { ProdutoService } from '../services/produto.service';
 export class NovoComponent implements OnInit {
 
   @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
+
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  canvasRotation = 0;
+  rotation = 0;
+  scale = 1;
+  showCropper = false;
+  containWithinAspectRatio = false;
+  transform: ImageTransform = {};
+  imageURL: string;
+  imagemNome: string;
 
   produto: Produto;
   fornecedores: Fornecedor[];
@@ -122,5 +134,25 @@ export class NovoComponent implements OnInit {
     this.errors = fail.error.errors;
     this.toastr.error('Ocorreu um erro!', 'Opa :(');
   }
-}
 
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+    this.imagemNome = event.currentTarget.files[0].name;
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+  }
+
+  imageLoaded() {
+    this.showCropper = true;
+  }
+
+  cropperReady(sourceImageDimensions: Dimensions) {
+    console.log('Cropper ready', sourceImageDimensions);
+  }
+
+  loadImageFailed() {
+    this.errors.push('O formato do arquivo ' + this.imagemNome + ' não é aceito');
+  }
+}
