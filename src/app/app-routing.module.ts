c513bbf7e02b656e88baa12af42c226d1454f04d
Bryan <bryanlima200@outlook.com>
2022-11-15 21:50:09 -0300

    Estrutura inicial do módulo Produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'conta', loadChildren: () => import('./conta/conta.module').then(m => m.ContaModule) },
   { path: 'fornecedores', loadChildren: () => import('./fornecedor/fornecedor.module').then(m => m.FornecedorModule) },
+  { path: 'produtos', loadChildren: () => import('./produto/produto.module').then(m => m.ProdutoModule) },
   { path: 'acesso-negado', component: AcessoNegadoComponent },
   { path: 'nao-encontrado', component: NotFoundComponent },
   { path: '**', component: NotFoundComponent }
